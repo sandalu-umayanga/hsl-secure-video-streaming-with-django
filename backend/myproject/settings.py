eb71faf34f020fc82395918e123647cc7a070791
Sandalu Umayanga <sandaluumayanga2002@gmail.com>
2026-02-11 12:25:41 +0530

    Fix backend Dockerfile syntax and enable permissive CORS for K8s testing

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -57,9 +57,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+# ]
 
 
 ROOT_URLCONF = 'myproject.urls'
